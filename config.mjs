a686d437897447ae2658290c55e4179a76401d83
Yusuf DKV <111687234+yusufexpert@users.noreply.github.com>
2023-05-01 17:48:19 +0700

    Add files via upload

---
+++ config.mjs
@@ -0,0 +1,14 @@
+global.owner = {
+	name: 'Yusuf',
+	number: '6283873115706',
+	numberId: '6283873115706@c.us',
+	ig: 'yusuf.dkv'
+}
+
+global.bot = {
+	name: 'Reyna',
+	name2: 'ReynaBOT',
+	name3: '© ReynaBOT',
+	number: '6283830393361',
+	numberId: '6283830393361@c.us'
+}
